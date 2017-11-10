59478af276c3020bb5640993e5ea811509cee4ab
Grant Hutchinson <grant@splorp.com>
2017-11-10 15:59:21 -0700

    Code formatting and tidying

--- lastfmrss.php
+++ lastfmrss.php
@@ -1,5 +1,6 @@
 <?php
 
+// http://simplehtmldom.sourceforge.net/
 require_once ('simple_html_dom.php');
 
 // Set the default Last.fm account to be used if no user is specified
@@ -23,25 +24,25 @@ header("Content-type: text/xml; charset=utf-8");
 ?>
 
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-        <lastBuildDate><?php echo gmdate(DATE_RFC822, time()) ?></lastBuildDate>
-        <language>en</language>
-        <ttl>960</ttl>
-        <category>Personal</category>
+	<channel>
+		<lastBuildDate><?php echo gmdate(DATE_RFC822, time()) ?></lastBuildDate>
+		<language>en</language>
 		<title>Last.fm : <?php echo $user ?> just <?php echo $type ?></title>
 		<description>
 			Last.fm : <?php echo $user ?> just <?php echo $type ?>
 		</description>
 		<link>http://www.last.fm/user/<?php echo $user ?></link>
+		<ttl>960</ttl>
 		<generator>lastfmrss</generator>
+		<category>Personal</category>
 <?php
 
 $i = 0;
 foreach($html->find('.js-focus-controls-container') as $row) {
 	foreach($row->find('.chartlist-name') as $content) {
 		$artist = $content->find('a',0)->plaintext;
--		$title = $content->find('a',1)->plaintext;
--		$link = $content->find('a',1)->href;
+		$title = $content->find('a',1)->plaintext;
+		$link = $content->find('a',1)->href;
 
 		$desc = 'https://www.last.fm'. $link;
 		$desc = '<a href="'.$desc.'">'.$artist.'</a>';
@@ -55,20 +56,18 @@ foreach($html->find('.js-focus-controls-container') as $row) {
 
  		$playdate = gmdate(DATE_RFC822, strtotime($da_time));
 	}
-	?>
-  <item>
-  	<title><?php echo $artist.' - '.$title ?> </title>
-  	<pubDate><?php echo $playdate; ?></pubDate>
-  	<link>http://www.last.fm<?php echo $link ?></link>
-    <guid isPermaLink='false'><?php echo $link ?></guid>
-  	<description><![CDATA[<?php echo $desc?>]]></description>
-  </item>
+?>
+		<item>
+			<title><?php echo $artist.' - '.$title ?> </title>
+			<pubDate><?php echo $playdate; ?></pubDate>
+			<link>http://www.last.fm<?php echo $link ?></link>
+			<guid isPermaLink='false'><?php echo $link ?></guid>
+			<description><![CDATA[<?php echo $desc?>]]></description>
+		</item>
 
-	<?php
-	//echo "{$i}. {$artist} - {$title} \n {$desc}\n";
+<?php
 	$i++;
 }
-
 ?>
 	</channel>
 </rss>
